20d2407a940f57d6e5d4a514c8aa1fbecbddf12c
alimohress <alimohress@gmail.com>
2021-07-22 00:56:22 +0430

    tests: add request and response serializers

--- tests/serializers.py
+++ tests/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import ExampleModel
 
 
-class ExmapleSerializer(serializers.ModelSerializer):
+class ExmapleReqSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExampleModel
+        fields = '__all__'
+        execlude = ['id']
+
+
+class ExampleResSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExampleModel
         fields = '__all__'
